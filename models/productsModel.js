420182caee2227542c7604cc60f583a8254a5df6
Charles <enagecharles27@gmail.com>
2026-01-30 01:39:06 +0800

    productsModel

---
+++ models/productsModel.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+const schema = mongoose.Schema;
+
+const productSchema = new schema(
+  {
+    name: String,
+    imageUrl: {
+      type: String,
+    },
+    category: String,
+    price: Number,
+  },
+  { timestamps: true },
+);
+
+export default mongoose.model("product", productSchema);
