04f352f092ff73b410fc841a2ed8ebb9b21f36d4
Charles <enagecharles27@gmail.com>
2026-01-31 01:28:47 +0800

    Product Routes

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,11 +1,12 @@
 import express from "express";
+import { upload } from "../middleware/multer.js";
 import {
   addProduct,
   getAllProducts,
 } from "../controllers/productController.js";
 const router = express.Router();
 
-router.post("/add", addProduct);
+router.post("/add", upload.single("image"), addProduct);
 
 router.get("/", getAllProducts);
 
