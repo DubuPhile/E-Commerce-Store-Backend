6233497c48ac46771430e03585036b07bdba1995
Charles <enagecharles27@gmail.com>
2026-01-31 01:27:53 +0800

    Optimize verifyJWT

--- middleware/verifyJWT.js
+++ middleware/verifyJWT.js
@@ -1,7 +1,7 @@
 import jwt from "jsonwebtoken";
 
-const verifyJWT = (res, req, next) => {
-  const authHeader = req.headers.Authorization || req.headers.authorization;
+const verifyJWT = (req, res, next) => {
+  const authHeader = req.headers.authorization || req.headers.Authorization;
   if (!authHeader?.startsWith("Bearer ")) return res.sendStatus(401);
   const token = authHeader.split(" ")[1];
   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
