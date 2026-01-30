62b595449ae5981ece565ec34bf3f7e9089ba9ef
Charles <enagecharles27@gmail.com>
2026-01-31 01:28:12 +0800

    CartModel

---
+++ models/CartModel.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+const schema = mongoose.Schema;
+
+const cartSchema = new schema({
+  user: {
+    type: schema.Types.ObjectId,
+    ref: "user",
+    required: true,
+  },
+  items: [
+    {
+      product: {
+        type: schema.Types.ObjectId,
+        ref: "product", // 👈 important
+        required: true,
+      },
+      quantity: {
+        type: Number,
+        default: 1,
+      },
+    },
+  ],
+});
+
+export default mongoose.model("Cart", cartSchema);
