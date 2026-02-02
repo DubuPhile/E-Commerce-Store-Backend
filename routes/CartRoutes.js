ecee9ac786a4e71bf37a3c3b42761084b732d381
Charles <enagecharles27@gmail.com>
2026-02-03 00:19:58 +0800

    Change Quantity routes

--- routes/CartRoutes.js
+++ routes/CartRoutes.js
@@ -3,6 +3,7 @@ import {
   addToCart,
   getMyCart,
   deleteItem,
+  changeQuantity,
 } from "../controllers/cartController.js";
 import verifyJWT from "../middleware/verifyJWT.js";
 
@@ -12,5 +13,7 @@ router.post("/add-to-cart", verifyJWT, addToCart);
 router.get("/get-my-cart", verifyJWT, getMyCart);
 
 router.delete("/delete-item/:productId", verifyJWT, deleteItem);
+
+router.put("/changeQty/:itemId", verifyJWT, changeQuantity);
 
 export default router;
