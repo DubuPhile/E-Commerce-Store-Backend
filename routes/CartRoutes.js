f1e1d782f15a9330875b391c7db133c8cfa2c66d
Charles <enagecharles27@gmail.com>
2026-02-02 21:55:31 +0800

    delete item routes

--- routes/CartRoutes.js
+++ routes/CartRoutes.js
@@ -1,10 +1,16 @@
 import express from "express";
-import { addToCart, getMyCart } from "../controllers/cartController.js";
+import {
+  addToCart,
+  getMyCart,
+  deleteItem,
+} from "../controllers/cartController.js";
 import verifyJWT from "../middleware/verifyJWT.js";
 
 const router = express.Router();
 
 router.post("/add-to-cart", verifyJWT, addToCart);
 router.get("/get-my-cart", verifyJWT, getMyCart);
+
+router.delete("/delete-item/:productId", verifyJWT, deleteItem);
 
 export default router;
