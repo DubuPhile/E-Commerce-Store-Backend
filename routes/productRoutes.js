7b090aa7d2a68a37825111e73776a33fb0680e52
Charles <enagecharles27@gmail.com>
2026-02-04 00:35:17 +0800

    implement searh and get-product routes

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -3,11 +3,17 @@ import { upload } from "../middleware/multer.js";
 import {
   addProduct,
   getAllProducts,
+  searchProducts,
+  getProductDetails,
 } from "../controllers/productController.js";
 const router = express.Router();
 
 router.post("/add", upload.single("image"), addProduct);
 
 router.get("/", getAllProducts);
+
+router.get("/search", searchProducts);
+
+router.get("/get-product/:Id", getProductDetails);
 
 export default router;
