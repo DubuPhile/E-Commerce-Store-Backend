18c3ad232de3e31779bc3523fa0756e2fced5196
Charles <enagecharles27@gmail.com>
2026-02-24 00:19:48 +0800

    add comments in user routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,30 +5,30 @@ import { upload } from "../middleware/multer.js";
 import { firebaseLogin, setPassword } from "../controllers/firebaseLogin.js";
 const router = express.Router();
 
-router.post("/register", userController.registerUser);
+router.post("/register", userController.registerUser); //register User Routes
 
-router.post("/login", userController.LoginUser);
+router.post("/login", userController.LoginUser); //Login Routes
 
-router.post("/firebase-login", firebaseLogin);
+router.post("/firebase-login", firebaseLogin); //Social Media/firebase Login Routes
 
-router.post("/logout", userController.logoutUser);
+router.post("/logout", userController.logoutUser); //log out routes
 
-router.get("/getUser", verifyJWT, userController.getUser);
+router.get("/getUser", verifyJWT, userController.getUser); //Get User Details Routes
 
 router.put(
   "/updateUser",
   upload.single("image"),
   verifyJWT,
   userController.updateUser,
-);
+); // Update User ROutes
 
-router.patch("/changePwd", verifyJWT, userController.changePwd);
+router.patch("/changePwd", verifyJWT, userController.changePwd); //change Password Routes
 
-router.patch("/set-password", verifyJWT, setPassword);
+router.patch("/set-password", verifyJWT, setPassword); //Set Password Routes
 
 //OTP routes
-router.post("/change-password/send-otp", verifyJWT, userController.sendOTP);
+router.post("/change-password/send-otp", verifyJWT, userController.sendOTP); // Send OTP
 
-router.post("/verify-otp", verifyJWT, userController.verifyOTP);
+router.post("/verify-otp", verifyJWT, userController.verifyOTP); // Verify OTP
 
 export default router;
