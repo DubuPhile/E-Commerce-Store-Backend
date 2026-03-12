0e7fd85ee3b6bd1e16e913167a1e0d7f385f95f3
Charles <enagecharles27@gmail.com>
2026-03-13 00:15:00 +0800

    payment confirm routes

--- routes/paymentRoutes.js
+++ routes/paymentRoutes.js
@@ -1,8 +1,13 @@
 import express from "express";
-import { createPaymentIntent } from "../controllers/paymentController.js";
+import {
+  confirmPayment,
+  createPaymentIntent,
+} from "../controllers/paymentController.js";
 import verifyJWT from "../middleware/verifyJWT.js";
 const router = express.Router();
 
 router.post("/payment-intent", verifyJWT, createPaymentIntent);
+
+router.patch("/payment-confirm", verifyJWT, confirmPayment);
 
 export default router;
