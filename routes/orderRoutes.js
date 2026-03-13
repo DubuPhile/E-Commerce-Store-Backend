937fed8338fa10122b132caa32b72eb72b141fd6
Charles <enagecharles27@gmail.com>
2026-03-14 03:11:49 +0800

    checkout order routes

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -1,7 +1,13 @@
 import express from "express";
-import { confirmOrder, GetOrders } from "../controllers/orderController.js";
+import {
+  checkoutOrder,
+  confirmOrder,
+  GetOrders,
+} from "../controllers/orderController.js";
 import verifyJWT from "../middleware/verifyJWT.js";
 const router = express.Router();
+
+router.post("/checkout-order", verifyJWT, checkoutOrder);
 
 router.post("/confirm-order", verifyJWT, confirmOrder);
 
