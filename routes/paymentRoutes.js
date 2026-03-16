7a931560fae71e34fc95302f89d3d41dd145e206
Charles <enagecharles27@gmail.com>
2026-03-17 01:28:23 +0800

    stripe webhook routes

--- routes/paymentRoutes.js
+++ routes/paymentRoutes.js
@@ -2,6 +2,7 @@ import express from "express";
 import {
   confirmPayment,
   createPaymentIntent,
+  stripeWebhookController,
 } from "../controllers/paymentController.js";
 import verifyJWT from "../middleware/verifyJWT.js";
 const router = express.Router();
@@ -9,5 +10,11 @@ const router = express.Router();
 router.post("/payment-intent", verifyJWT, createPaymentIntent);
 
 router.patch("/payment-confirm", verifyJWT, confirmPayment);
+
+router.post(
+  "/stripe-webhook",
+  express.raw({ type: "application/json" }),
+  stripeWebhookController,
+);
 
 export default router;
