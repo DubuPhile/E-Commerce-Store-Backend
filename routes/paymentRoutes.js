14529a98b5d1047fa2a0aa4c7fd6f170981f2973
Charles <enagecharles27@gmail.com>
2026-03-18 00:04:31 +0800

    Optimize payment routes

--- routes/paymentRoutes.js
+++ routes/paymentRoutes.js
@@ -1,20 +1,8 @@
 import express from "express";
-import {
-  confirmPayment,
-  createPaymentIntent,
-  stripeWebhookController,
-} from "../controllers/paymentController.js";
+import { createPaymentIntent } from "../controllers/paymentController.js";
 import verifyJWT from "../middleware/verifyJWT.js";
 const router = express.Router();
 
 router.post("/payment-intent", verifyJWT, createPaymentIntent);
-
-router.patch("/payment-confirm", verifyJWT, confirmPayment);
-
-router.post(
-  "/stripe-webhook",
-  express.raw({ type: "application/json" }),
-  stripeWebhookController,
-);
 
 export default router;
